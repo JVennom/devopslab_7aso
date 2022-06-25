d77bd95ec34d30ddea4c3da169eb8f27ec3fcefc
JVennom <105567595+JVennom@users.noreply.github.com>
2022-06-25 19:43:47 -0300

    Corrigindo Docker

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "HelLo JhoNeS"
+    return "HelLo JhoNeS (docker)"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
